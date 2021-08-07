b11722cf163da7ec11612d82b63d68b4b6771e81
Liam Murphy <liampm32@gmail.com>
2021-08-07 16:17:47 +1000

    Graphics!

---
+++ src/lib.rs
@@ -0,0 +1,354 @@
+use std::convert::TryInto;
+use std::fmt;
+use std::fmt::Debug;
+use std::fmt::Display;
+use std::fmt::Formatter;
+use std::fmt::Write;
+use std::hash::Hash;
+use std::hash::Hasher;
+use std::iter::FromIterator;
+use std::ops::Shl;
+use std::ops::Shr;
+
+#[cfg(test)]
+mod tests;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Cell {
+    None,
+    X,
+    O,
+}
+
+impl Cell {
+    fn from_digit(digit: u32) -> Self {
+        match digit {
+            0 => Self::None,
+            1 => Self::X,
+            2 => Self::O,
+            _ => unreachable!(),
+        }
+    }
+}
+
+pub enum Win {
+    /// A win which is entirely located along the ring.
+    Ring {
+        /// The index in the ring at which this win starts; the two cells after it are also part of the win.
+        index: u8,
+    },
+    /// A win which goes through the center.
+    Center {
+        /// The index of one of the cells on the ring which forms this win; the other one is on the opposite side of the ring.
+        index: u8,
+    },
+}
+
+pub struct Board {
+    pub center: Cell,
+    pub ring: Ring,
+}
+
+impl Board {
+    /// Create a new, blank board with `cells` around the outside.
+    pub fn new(cells: u8) -> Self {
+        Self {
+            center: Cell::None,
+            ring: Ring::new(cells),
+        }
+    }
+
+    pub fn winner(&self) -> Cell {
+        // The `.cycle().take(10)` means that we put the first two on the end as well,
+        // so that we pick up matches on the wrapping-around point.
+        for cells in self
+            .ring
+            .into_iter()
+            .cycle()
+            .take(10)
+            .collect::<Vec<_>>()
+            .windows(3)
+        {
+            if cells == [Cell::X; 3] {
+                return Cell::X;
+            } else if cells == [Cell::O; 3] {
+                return Cell::O;
+            }
+        }
+
+        if self.center == Cell::None {
+            // If the middle is blank, there can't be a win through the middle.
+            return Cell::None;
+        }
+
+        debug_assert!(self.ring.cells % 2 == 0);
+
+        // Iterate over the pairs of cells on opposite sides of the board,
+        // by offsetting the second iterator by half.
+        for (a, b) in self
+            .ring
+            .into_iter()
+            .zip(self.ring.into_iter().skip((self.ring.cells / 2).into()))
+        {
+            if a == self.center && b == self.center {
+                return self.center;
+            }
+        }
+
+        Cell::None
+    }
+
+    /// Get all of the ways in which the game has been won.
+    pub fn wins(&self) -> Vec<Win> {
+        let mut out = Vec::new();
+
+        // The `.cycle().take(10)` means that we put the first two on the end as well,
+        // so that we pick up matches on the wrapping-around point.
+        for (i, cells) in self
+            .ring
+            .into_iter()
+            .cycle()
+            .take(10)
+            .collect::<Vec<_>>()
+            .windows(3)
+            .enumerate()
+        {
+            if cells == [Cell::X; 3] || cells == [Cell::O; 3] {
+                out.push(Win::Ring {
+                    index: i.try_into().expect("too many cells"),
+                })
+            }
+        }
+
+        debug_assert!(self.ring.cells % 2 == 0);
+
+        if self.center != Cell::None {
+            // Iterate over the pairs of cells on opposite sides of the board,
+            // by offsetting the second iterator by half.
+            for (i, (a, b)) in self
+                .ring
+                .into_iter()
+                .zip(self.ring.into_iter().skip((self.ring.cells / 2).into()))
+                .enumerate()
+            {
+                if a == self.center && b == self.center {
+                    out.push(Win::Center {
+                        index: i.try_into().expect("too many cells"),
+                    })
+                }
+            }
+        }
+
+        out
+    }
+}
+
+/// This is represented internally as a ternary integer, where 0 is an empty cell, 1 is an X, and 2 is an O.
+#[derive(Clone, Copy)]
+pub struct Ring {
+    // 32 bits is big enough to store rings of up to 20 cells. I'd say that's a reasonable limit.
+    // It'd be possible to store it a bit more efficiently by enumerating all the boards, but eh.
+    int: u32,
+
+    // This should be at most 20 to work properly.
+    cells: u8,
+}
+
+impl Ring {
+    pub fn new(cells: u8) -> Self {
+        Self { int: 0, cells }
+    }
+
+    pub fn canonicalise(self) -> Self {
+        let max = (0..self.cells)
+            .map(|n| self << n)
+            .flat_map(|ring| [ring, ring.reverse()])
+            .max_by_key(|ring| ring.int);
+        max.unwrap()
+    }
+
+    pub fn len(&self) -> u8 {
+        self.cells
+    }
+
+    pub fn get(&self, i: u8) -> Cell {
+        Cell::from_digit(self.int / 3u32.pow((self.cells - i - 1).into()) % 3)
+    }
+
+    pub fn set(&mut self, i: u8, cell: Cell) {
+        let multiplier = 3u32.pow((self.cells - i - 1).into());
+
+        // Apply the difference between the value of the existing digit there and the new digit.
+        let digit = self.int / multiplier % 3;
+        let new_digit = match cell {
+            Cell::None => 0,
+            Cell::X => 1,
+            Cell::O => 2,
+        };
+        let diff = new_digit - digit as i32;
+        // Signed and unsigned addition are actually the same operation, so just pretend this is a `u32` to make the compiler let us do this.
+        self.int = self.int.wrapping_add((diff * multiplier as i32) as u32);
+    }
+
+    fn reverse(self) -> Self {
+        // I can't think of any fancier way of doing this.
+        self.into_iter().rev().collect()
+    }
+}
+
+// This doesn't really behave the same as a bit-shift, since it wraps around to the other side, but it's fine.
+impl Shl<u8> for Ring {
+    type Output = Self;
+
+    fn shl(self, rhs: u8) -> Self::Output {
+        let rhs = rhs % self.cells;
+
+        // We want any bits which go off the end of the number to be truncated, which is all that wrapping really is.
+        let truncated = self.int.wrapping_mul(3u32.pow(rhs.into())) % 3u32.pow(self.cells.into());
+        let wrapped = self.int / 3u32.pow((self.cells - rhs).into());
+        Self {
+            int: truncated + wrapped,
+            cells: self.cells,
+        }
+    }
+}
+
+impl Shr<u8> for Ring {
+    type Output = Self;
+
+    fn shr(self, rhs: u8) -> Self::Output {
+        let rhs = rhs % self.cells;
+        // The digits which are getting wrapped.
+        let mut wrapped = self.int % 3u32.pow(rhs.into());
+        // Move them up to the most significant digits where they'll end up.
+        wrapped *= 3u32.pow((self.cells - rhs).into());
+
+        let truncated = self.int / 3u32.pow(rhs.into());
+
+        Self {
+            int: truncated + wrapped,
+            cells: self.cells,
+        }
+    }
+}
+
+impl Hash for Ring {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        state.write_u32(self.canonicalise().int);
+    }
+}
+
+impl PartialEq for Ring {
+    fn eq(&self, other: &Self) -> bool {
+        self.canonicalise().int == other.canonicalise().int
+    }
+}
+
+impl Eq for Ring {}
+
+impl Debug for Ring {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        <Self as Display>::fmt(self, f)
+    }
+}
+
+impl Display for Ring {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        for cell in self.into_iter() {
+            f.write_char(match cell {
+                Cell::None => ' ',
+                Cell::X => 'X',
+                Cell::O => 'O',
+            })?;
+        }
+        Ok(())
+    }
+}
+
+impl FromIterator<Cell> for Ring {
+    fn from_iter<T: IntoIterator<Item = Cell>>(iter: T) -> Self {
+        let mut int = 0;
+        let mut cells = 0;
+        for cell in iter {
+            cells += 1;
+            int *= 3;
+
+            int += match cell {
+                Cell::None => 0,
+                Cell::X => 1,
+                Cell::O => 2,
+            }
+        }
+        debug_assert!(cells <= 20);
+        Self { int, cells }
+    }
+}
+
+impl IntoIterator for Ring {
+    type Item = Cell;
+
+    type IntoIter = Cells;
+
+    fn into_iter(self) -> Self::IntoIter {
+        Cells {
+            int: self.int,
+            denom: 3u32.pow((self.cells - 1).into()),
+        }
+    }
+}
+
+#[derive(Clone)]
+pub struct Cells {
+    int: u32,
+    denom: u32,
+}
+
+impl Iterator for Cells {
+    type Item = Cell;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.denom == 0 {
+            None
+        } else {
+            let digit = self.int / self.denom % 3;
+            self.int %= self.denom;
+            self.denom /= 3;
+
+            Some(Cell::from_digit(digit))
+        }
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        let len = self.len();
+        (len, Some(len))
+    }
+}
+
+impl DoubleEndedIterator for Cells {
+    fn next_back(&mut self) -> Option<Self::Item> {
+        if self.denom == 0 {
+            None
+        } else {
+            let digit = self.int % 3;
+            self.int /= 3;
+            self.denom /= 3;
+
+            Some(Cell::from_digit(digit))
+        }
+    }
+}
+
+impl ExactSizeIterator for Cells {
+    fn len(&self) -> usize {
+        // There's no log for ints, so just count the number of times we have to divide `denom` by 3 for it to reach 0.
+        // (This might actually be less efficient than converting to a float for all I know, but that just seems kinda icky.)
+
+        let mut denom = self.denom;
+        let mut count = 0;
+        while denom > 0 {
+            denom /= 3;
+            count += 1;
+        }
+        count
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,269 +1,291 @@
-use std::collections::HashSet;
-use std::fmt;
-use std::fmt::Debug;
-use std::fmt::Display;
-use std::fmt::Formatter;
-use std::fmt::Write;
-use std::hash::Hash;
-use std::hash::Hasher;
-use std::iter::FromIterator;
-use std::ops::Shl;
-use std::ops::Shr;
+use std::f32::consts::FRAC_1_SQRT_2;
+use std::f32::consts::PI;
+use std::f32::consts::TAU;
 
-#[cfg(test)]
-mod tests;
+use macroquad::prelude::*;
+use ringtactoe::Board;
+use ringtactoe::Cell;
+use ringtactoe::Win;
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-enum Cell {
-    None,
-    X,
-    O,
+const RADIUS: f32 = 300.0;
+const CENTER_RADIUS: f32 = 100.0;
+const CELL_RADIUS: f32 = CENTER_RADIUS * 2.0 / 3.0;
+const LINE_THICKNESS: f32 = 4.0;
+const CELL_COLOR: Color = WHITE;
+// gap between inner circle and ring
+const GAP: f32 = 5.0;
+
+// l = r * angle
+// angle = l / r
+const INNER_GAP_ANGLE: f32 = GAP / (CENTER_RADIUS + GAP);
+const OUTER_GAP_ANGLE: f32 = GAP / RADIUS;
+const MIDDLE_GAP_ANGLE: f32 = (INNER_GAP_ANGLE + OUTER_GAP_ANGLE) / 2.0;
+
+const RING_BG: Color = LIME;
+
+fn draw_cell(x: f32, y: f32, rotation: f32, cell: Cell) {
+    match cell {
+        Cell::None => {}
+        Cell::X => {
+            // The messy working out for all this nonsense is in `working.heic`.
+
+            let cos = rotation.cos();
+            let sin = rotation.sin();
+
+            let off1 = CELL_RADIUS * (sin + cos) * FRAC_1_SQRT_2;
+            let off2 = CELL_RADIUS * (sin - cos) * FRAC_1_SQRT_2;
+
+            draw_line(
+                x - off1,
+                y - off2,
+                x + off1,
+                y + off2,
+                LINE_THICKNESS,
+                CELL_COLOR,
+            );
+
+            draw_line(
+                x + off2,
+                y - off1,
+                x - off2,
+                y + off1,
+                LINE_THICKNESS,
+                CELL_COLOR,
+            );
+        }
+        Cell::O => {
+            draw_poly_lines(x, y, 100, CELL_RADIUS, rotation, LINE_THICKNESS, CELL_COLOR);
+        }
+    }
 }
 
-struct Board {
-    center: Cell,
-    ring: Ring,
+fn draw_board(board: &Board, rotation: f32) {
+    let center_x = screen_width() / 2.0;
+    let center_y = screen_height() / 2.0;
+
+    // First, just draw the cell in the middle.
+    draw_poly(center_x, center_y, 100, CENTER_RADIUS, 0.0, RING_BG);
+    draw_cell(center_x, center_y, 0.0, board.center);
+
+    // Drawing the ring around the outside is a bit more complicated, since macroquad doesn't provide any way of drawing arcs or anything.
+    // So instead, we just have to draw all the individual triangles ourselves.
+    for (i, cell) in board.ring.into_iter().enumerate() {
+        let num_cells = board.ring.len() as f32;
+        let angle = rotation + i as f32 / num_cells * TAU;
+        let arc = TAU / num_cells;
+        let inner_arc = arc - INNER_GAP_ANGLE;
+        let outer_arc = arc - OUTER_GAP_ANGLE;
+
+        let points: Vec<_> = (0..=20)
+            .flat_map(|i| {
+                let inner_angle = angle + inner_arc / 20.0 * (i as f32);
+                let outer_angle = angle + outer_arc / 20.0 * (i as f32);
+
+                [
+                    vec2(
+                        center_x + outer_angle.cos() * RADIUS,
+                        center_y + outer_angle.sin() * RADIUS,
+                    ),
+                    vec2(
+                        center_x + inner_angle.cos() * (CENTER_RADIUS + GAP),
+                        center_y + inner_angle.sin() * (CENTER_RADIUS + GAP),
+                    ),
+                ]
+            })
+            .collect();
+
+        // TODO: replace this with `array_windows` when it's stabilised.
+        for window in points.windows(3) {
+            let (a, b, c) = match window {
+                [a, b, c] => (a, b, c),
+                _ => unreachable!(),
+            };
+
+            draw_triangle(*a, *b, *c, RING_BG);
+        }
+
+        // Since we're putting the cell in the middle, take the average of the inner and outer arcs.
+        let cell_angle = angle + (inner_arc + outer_arc) / 4.0;
+
+        const RADIUS_DIFF: f32 = RADIUS - (CENTER_RADIUS + GAP);
+        const CELL_POS_RADIUS: f32 = CENTER_RADIUS + GAP + (RADIUS_DIFF / 2.0);
+
+        draw_cell(
+            center_x + CELL_POS_RADIUS * cell_angle.cos(),
+            center_y + CELL_POS_RADIUS * cell_angle.sin(),
+            cell_angle,
+            cell,
+        );
+    }
+
+    for win in board.wins() {
+        match win {
+            Win::Center { index } => {
+                let num_cells = board.ring.len() as f32;
+
+                let angle = rotation + index as f32 / num_cells * TAU;
+                let arc = TAU / num_cells - MIDDLE_GAP_ANGLE;
+
+                // Since we're putting the cell in the middle, take the average of the inner and outer arcs.
+                let angle = angle + arc / 2.0;
+
+                let x_off = RADIUS * angle.cos();
+                let y_off = RADIUS * angle.sin();
+
+                draw_line(
+                    center_x - x_off,
+                    center_y - y_off,
+                    center_x + x_off,
+                    center_y + y_off,
+                    LINE_THICKNESS * 2.0,
+                    RED,
+                );
+            }
+            Win::Ring { index } => {
+                let num_cells = board.ring.len() as f32;
+
+                let angle = rotation + index as f32 / num_cells * TAU;
+                let arc = TAU / num_cells * 3.0 - MIDDLE_GAP_ANGLE;
+
+                for i in 0..100 {
+                    let start_portion = i as f32 / 100.0;
+                    let end_portion = (i + 1) as f32 / 100.0;
+
+                    let start_angle = angle + start_portion * arc;
+                    let end_angle = angle + end_portion * arc;
+
+                    const RADIUS_DIFF: f32 = RADIUS - (CENTER_RADIUS + GAP);
+                    const RING_CENTER_RADIUS: f32 = CENTER_RADIUS + GAP + (RADIUS_DIFF / 2.0);
+
+                    let start_x_off = RING_CENTER_RADIUS * start_angle.cos();
+                    let start_y_off = RING_CENTER_RADIUS * start_angle.sin();
+                    let end_x_off = RING_CENTER_RADIUS * end_angle.cos();
+                    let end_y_off = RING_CENTER_RADIUS * end_angle.sin();
+
+                    draw_line(
+                        center_x + start_x_off,
+                        center_y + start_y_off,
+                        center_x + end_x_off,
+                        center_y + end_y_off,
+                        LINE_THICKNESS * 2.0,
+                        RED,
+                    );
+                }
+            }
+        }
+    }
 }
 
-impl Board {
-    fn winner(&self) -> Cell {
-        let mut last_cell = Cell::None;
-        let mut num_seen: u8 = 0;
+#[macroquad::main("Ring-Tac-Toe")]
+async fn main() {
+    let mut board = Board::new(8);
 
-        // The `.cycle().take(10)` means that we put the first two on the end as well,
-        // so that we pick up matches on the wrapping-around point.
-        for cell in self.ring.cells().cycle().take(10) {
-            if cell == last_cell {
-                num_seen += 1;
-                if num_seen == 3 {
-                    return cell;
+    let mut turn = Cell::X;
+
+    let mut rotation = 0.0;
+    let mut velocity = 0.0;
+
+    // Becomes `Some` when the the ring is grabbed.
+    let mut last_mouse_angle = None;
+
+    let mut mouse_movement = 0.0;
+    let mut last_mouse_pos = (0.0, 0.0);
+
+    loop {
+        draw_board(&board, rotation);
+
+        let (mut x, mut y) = mouse_position();
+        x -= screen_width() / 2.0;
+        y -= screen_height() / 2.0;
+
+        // inverse tan is also called arctan, apparently
+        let mut angle = f32::atan(y / x);
+        // `atan` returns angles in the first quadrant for positive values and angles in the fourth quadrant for negative values.
+        // Fix the angle to be in the correct quadrant.
+        if x < 0.0 {
+            // If `angle` is negative, it will end up as PI - angle, in the second quadrant, which is correct.
+            // If `angle` is positive, it will end up as PI + angle, in the third quadrant, which is correct.
+            angle += PI;
+        }
+
+        if let Some(last_angle) = last_mouse_angle {
+            let diff = angle - last_angle;
+            if is_mouse_button_released(MouseButton::Left) {
+                last_mouse_angle = None;
+
+                // If the mouse was barely moved, we consider it a click.
+                if mouse_movement < 5.0 {
+                    // We already know they were clicking the ring, since `last_mouse_angle` was `Some`.
+
+                    // Undo the offset of the ring's rotation
+                    angle -= rotation;
+
+                    // Put all of the angles in the 0..TAU range.
+                    while angle < 0.0 {
+                        angle += TAU;
+                    }
+
+                    angle %= TAU;
+
+                    // Figure out which index in the ring the angle corresponds to.
+                    let i = f32::floor(angle / TAU * board.ring.len() as f32) as u8;
+
+                    // Set the cell.
+                    board.ring.set(i, turn);
+
+                    turn = match turn {
+                        Cell::X => Cell::O,
+                        Cell::O => Cell::X,
+                        Cell::None => unreachable!(),
+                    }
+                } else {
+                    // This was a drag, so give the ring the velocity that mouse had when it let go.
+                    velocity = diff / get_frame_time();
                 }
             } else {
-                num_seen = 1;
-                last_cell = cell;
+                // The mouse was already pressed, so we need to update the rotation to apply the change in mouse position.
+                rotation += diff;
+                last_mouse_angle = Some(angle);
             }
-        }
-
-        if self.center == Cell::None {
-            // If the middle is blank, there can't be a win through the middle.
-            return Cell::None;
-        }
-
-        debug_assert!(self.ring.cells % 2 == 0);
-
-        // Iterate over the pairs of cells on opposite sides of the board,
-        // by offsetting the second iterator by half.
-        for (a, b) in self.ring.cells().zip(self.ring.cells().skip((self.ring.cells / 2).into())) {
-            if a == self.center && b == self.center {
-                return self.center;
-            }
-        }
-
-        Cell::None
-    }
-}
-
-/// This is represented internally as a ternary integer, where 0 is an empty cell, 1 is an X, and 2 is an O.
-#[derive(Clone, Copy)]
-struct Ring {
-    // 32 bits is big enough to store rings of up to 20 cells. I'd say that's a reasonable limit.
-    // It'd be possible to store it a bit more efficiently by enumerating all the boards, but eh.
-    int: u32,
-
-    // This should be at most 20 to work properly.
-    cells: u8,
-}
-
-impl Ring {
-    fn canonicalise(self) -> Self {
-        let max = (0..self.cells)
-            .map(|n| self << n)
-            .flat_map(|ring| [ring, ring.reverse()])
-            .max_by_key(|ring| ring.int);
-        max.unwrap()
-    }
-
-    fn cells(self) -> Cells {
-        Cells {
-            int: self.int,
-            denom: 3u32.pow((self.cells - 1).into()),
-        }
-    }
-
-    fn reverse(self) -> Self {
-        // I can't think of any fancier way of doing this.
-        self.cells().rev().collect()
-    }
-}
-
-// This doesn't really behave the same as a bit-shift, since it wraps around to the other side, but it's fine.
-impl Shl<u8> for Ring {
-    type Output = Self;
-
-    fn shl(self, rhs: u8) -> Self::Output {
-        let rhs = rhs % self.cells;
-
-        // We want any bits which go off the end of the number to be truncated, which is all that wrapping really is.
-        let truncated = self.int.wrapping_mul(3u32.pow(rhs.into())) % 3u32.pow(self.cells.into());
-        let wrapped = self.int / 3u32.pow((self.cells - rhs).into());
-        Self {
-            int: truncated + wrapped,
-            cells: self.cells,
-        }
-    }
-}
-
-impl Shr<u8> for Ring {
-    type Output = Self;
-
-    fn shr(self, rhs: u8) -> Self::Output {
-        let rhs = rhs % self.cells;
-        // The digits which are getting wrapped.
-        let mut wrapped = self.int % 3u32.pow(rhs.into());
-        // Move them up to the most significant digits where they'll end up.
-        wrapped *= 3u32.pow((self.cells - rhs).into());
-
-        let truncated = self.int / 3u32.pow(rhs.into());
-
-        Self {
-            int: truncated + wrapped,
-            cells: self.cells,
-        }
-    }
-}
-
-impl Hash for Ring {
-    fn hash<H: Hasher>(&self, state: &mut H) {
-        state.write_u32(self.canonicalise().int);
-    }
-}
-
-impl PartialEq for Ring {
-    fn eq(&self, other: &Self) -> bool {
-        self.canonicalise().int == other.canonicalise().int
-    }
-}
-
-impl Eq for Ring {}
-
-impl Debug for Ring {
-    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        <Self as Display>::fmt(self, f)
-    }
-}
-
-impl Display for Ring {
-    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        for cell in self.cells() {
-            f.write_char(match cell {
-                Cell::None => ' ',
-                Cell::X => 'X',
-                Cell::O => 'O',
-            })?;
-        }
-        Ok(())
-    }
-}
-
-impl FromIterator<Cell> for Ring {
-    fn from_iter<T: IntoIterator<Item = Cell>>(iter: T) -> Self {
-        let mut int = 0;
-        let mut cells = 0;
-        for cell in iter {
-            cells += 1;
-            int *= 3;
-
-            int += match cell {
-                Cell::None => 0,
-                Cell::X => 1,
-                Cell::O => 2,
-            }
-        }
-        debug_assert!(cells <= 20);
-        Self { int, cells }
-    }
-}
-
-#[derive(Clone)]
-struct Cells {
-    int: u32,
-    denom: u32,
-}
-
-impl Iterator for Cells {
-    type Item = Cell;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.denom == 0 {
-            None
         } else {
-            let digit = self.int / self.denom % 3;
-            self.int %= self.denom;
-            self.denom /= 3;
+            if is_mouse_button_pressed(MouseButton::Left) {
+                mouse_movement = 0.0;
 
-            Some(match digit {
-                0 => Cell::None,
-                1 => Cell::X,
-                2 => Cell::O,
-                _ => unreachable!(),
-            })
+                let dist_from_center = f32::sqrt(x.powi(2) + y.powi(2));
+                // The click was within the ring, so mark it as grabbed.
+                if dist_from_center > CENTER_RADIUS + GAP && dist_from_center < RADIUS {
+                    last_mouse_angle = Some(angle);
+                }
+            } else if is_mouse_button_released(MouseButton::Left) {
+                // If the mouse was barely moved, we consider it a click.
+                if mouse_movement < 5.0 {
+                    // If this was a click on the ring, `last_mouse_angle` would have been `Some`, so this can only have been a click in the center.
+                    let dist_from_center = f32::sqrt(x.powi(2) + y.powi(2));
+                    if dist_from_center < CENTER_RADIUS {
+                        // They clicked the center.
+                        board.center = turn;
+
+                        turn = match turn {
+                            Cell::X => Cell::O,
+                            Cell::O => Cell::X,
+                            Cell::None => unreachable!(),
+                        }
+                    }
+                }
+            }
         }
-    }
 
-    fn size_hint(&self) -> (usize, Option<usize>) {
-        let len = self.len();
-        (len, Some(len))
-    }
-}
+        mouse_movement +=
+            f32::sqrt((x - last_mouse_pos.0).powi(2) + (y - last_mouse_pos.1).powi(2));
+        last_mouse_pos = (x, y);
 
-impl DoubleEndedIterator for Cells {
-    fn next_back(&mut self) -> Option<Self::Item> {
-        if self.denom == 0 {
-            None
-        } else {
-            let digit = self.int % 3;
-            self.int /= 3;
-            self.denom /= 3;
-
-            Some(match digit {
-                0 => Cell::None,
-                1 => Cell::X,
-                2 => Cell::O,
-                _ => unreachable!(),
-            })
+        // Don't just use an `else` block so that this also triggers on the frame that the mouse is released and this has just been set to `None`.
+        if last_mouse_angle.is_none() {
+            rotation += velocity * get_frame_time();
+            // Simulate friction
+            velocity *= 0.9;
         }
+
+        next_frame().await;
     }
-}
-
-impl ExactSizeIterator for Cells {
-    fn len(&self) -> usize {
-        // There's no log for ints, so just count the number of times we have to divide `denom` by 3 for it to reach 0.
-        // (This might actually be less efficient than converting to a float for all I know, but that just seems kinda icky.)
-
-        let mut denom = self.denom;
-        let mut count = 0;
-        while denom > 0 {
-            denom /= 3;
-            count += 1;
-        }
-        count
-    }
-}
-
-fn main() {
-    let mut set = HashSet::new();
-
-    for n in 0..3u32.pow(8) {
-        set.insert(Ring { int: n, cells: 8 }.canonicalise());
-    }
-
-    let mut rings: Vec<_> = set.into_iter().collect();
-
-    rings.sort_unstable_by_key(|ring| ring.int);
-
-    for ring in &rings {
-        println!("{}", ring);
-    }
-
-    println!("Number of unique tic-tac-toe rings: {}", rings.len());
 }
